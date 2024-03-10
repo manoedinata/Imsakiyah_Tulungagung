de12aaa647ae91008054d1c2160a0f6911270685
Hendra Manudinata <manoedinata@gmail.com>
2024-03-10 09:07:46 +0700

    Caption

--- instagram.py
+++ instagram.py
@@ -1,12 +1,18 @@
 from instagrapi import Client
 from instagrapi.exceptions import LoginRequired
 from instagrapi.types import Media
-
-import logging
 import os
+
+# Meng-import ini akan memulai proses pengambilan jadwal
+# It's not a bug, it's a feature :D (#canda)
+from imsakiyah import formattedDate
 
 USERNAME = os.environ.get("IG_USERNAME")
 PASSWORD = os.environ.get("IG_PASSWORD")
+
+# Caption
+with open("caption.txt", "r") as file:
+    caption = file.read().format(formattedDate)
 
 cl = Client()
 
@@ -18,7 +24,7 @@ files = [os.path.join("out", fn) for fn in next(os.walk("./out"))[2]]
 files.sort(key=lambda fname: int(fname.split('.')[0].split("/")[1]))
 
 print("Uploading...")
-upload: Media = cl.album_upload(files, "Tes boloo")
+upload: Media = cl.album_upload(files, caption)
 print(upload.code)
 
 cl.logout()
